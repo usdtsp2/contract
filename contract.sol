25305e876e862d8a32043a5f43321df9826ae48c
usdtsp2 <adminusdt@usdtsp2.co.ke>
2025-01-06 11:37:52 +0300

    update

--- contract.sol
+++ contract.sol
@@ -5,9 +5,9 @@ pragma solidity 0.8.0;
 interface ITRC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
